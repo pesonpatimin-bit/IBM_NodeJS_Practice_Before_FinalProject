86fbfbdcd3ddab6ec82b2afb35a935e46eec9ed6
Peson Patimin <peson.patimin@gmail.com>
2025-11-01 09:37:49 -0400

    2nd

--- router/friends.js
+++ router/friends.js
@@ -41,16 +41,44 @@ router.post("/", function(req, res) {
 
 
 // PUT request: Update the details of a friend with email id
-router.put("/:email", (req, res) => {
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.put("/:email", function(req, res) {
+    // Extract email parameter from request URL
+    const email = req.params.email;
+    let friend = friends[email];  // Retrieve friend object associated with email
+
+    if (friend) {  // Check if friend exists
+        let DOB = req.body.DOB;
+        // Add similarly for firstName
+        // Add similarly for lastName
+
+        // Update DOB if provided in request body
+        if (DOB) {
+            friend["DOB"] = DOB;
+        }
+        // Add similarly for firstName
+        // Add similarly for lastName
+
+        friends[email] = friend;  // Update friend details in 'friends' object
+        res.send(`Friend with the email ${email} updated.`);
+    } else {
+        // Respond if friend with specified email is not found
+        res.send("Unable to find friend!");
+    }
 });
 
 
 // DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract email parameter from request URL
+    const email = req.params.email;
+
+    if (email) {
+        // Delete friend from 'friends' object based on provided email
+        delete friends[email];
+    }
+    
+    // Send response confirming deletion of friend
+    res.send(`Friend with the email ${email} deleted.`);
 });
 
 module.exports=router;
